a1dce3c35eae87e6bd88c2159d76030c0b7e3e96
Melih Yayli <47675833+mlhyyl@users.noreply.github.com>
2020-12-04 11:12:31 +0300

    Redirect links to pass through IC website

--- rightclick.js
+++ rightclick.js
@@ -6,35 +6,49 @@ chrome.extension.onRequest.addListener(function (request) {
 });
 
 function catalogAuthor(selectedText) {
-    var serviceCall = 'https://primo-90su.hosted.exlibrisgroup.com/primo-explore/search?query=creator,contains,' + selectedText + ',AND&tab=default_tab&search_scope=Everything&vid=90SU_VIEW&mode=simple&offset=0';
+    // var serviceCall = 'https://primo-90su.hosted.exlibrisgroup.com/primo-explore/search?query=creator,contains,' + selectedText + ',AND&tab=default_tab&search_scope=Everything&vid=90SU_VIEW&mode=simple&offset=0';
+    var serviceCall = 'https://www.sabanciuniv.edu/bm/en/search-redirect?type=cc&term=a&value=' + selectedText;
     chrome.tabs.create({
         url: serviceCall
     });
+    trackAction();
 }
 
 function catalogTitle(selectedText) {
-    var serviceCall = 'https://primo-90su.hosted.exlibrisgroup.com/primo-explore/search?query=title,contains,' + selectedText + ',AND&tab=default_tab&search_scope=Everything&vid=90SU_VIEW&mode=simple&offset=0';
+    // var serviceCall = 'https://primo-90su.hosted.exlibrisgroup.com/primo-explore/search?query=title,contains,' + selectedText + ',AND&tab=default_tab&search_scope=Everything&vid=90SU_VIEW&mode=simple&offset=0';
+    var serviceCall = 'https://www.sabanciuniv.edu/bm/en/search-redirect?type=cc&term=t&value=' + selectedText;
     chrome.tabs.create({
         url: serviceCall
     });
 }
 
-function catalogKeyword(selectedText) {
-    var serviceCall = 'https://primo-90su.hosted.exlibrisgroup.com/primo-explore/search?query=sub,contains,' + selectedText + ',AND&tab=default_tab&search_scope=Everything&vid=90SU_VIEW&mode=simple&offset=0';
+function catalogSubject(selectedText) {
+    // var serviceCall = 'https://primo-90su.hosted.exlibrisgroup.com/primo-explore/search?query=sub,contains,' + selectedText + ',AND&tab=default_tab&search_scope=Everything&vid=90SU_VIEW&mode=simple&offset=0';
+    var serviceCall = 'https://www.sabanciuniv.edu/bm/en/search-redirect?type=cc&term=k&value=' + selectedText;
+    chrome.tabs.create({
+        url: serviceCall
+    });
+}
+
+function catalogAny(selectedText) {
+    // var serviceCall = 'https://primo-90su.hosted.exlibrisgroup.com/primo-explore/search?query=any,contains,' + selectedText + ',AND&tab=default_tab&search_scope=Everything&vid=90SU_VIEW&mode=simple&offset=0';
+    var serviceCall = 'https://www.sabanciuniv.edu/bm/en/search-redirect?type=cc&term=af&value=' + selectedText;
     chrome.tabs.create({
         url: serviceCall
     });
 }
 
 function journalStarts(selectedText) {
-    var serviceCall = 'https://sfx-90su.hosted.exlibrisgroup.com/sabanci/journalsearch?param_pattern_value=' + selectedText + '&param_textSearchType_value=startsWith';
+    // var serviceCall = 'https://sfx-90su.hosted.exlibrisgroup.com/sabanci/journalsearch?param_pattern_value=' + selectedText + '&param_textSearchType_value=startsWith';
+    var serviceCall = 'https://www.sabanciuniv.edu/bm/en/search-redirect?type=j&term=s&value=' + selectedText;
     chrome.tabs.create({
         url: serviceCall
     });
 }
 
 function journalContains(selectedText) {
-    var serviceCall = 'https://sfx-90su.hosted.exlibrisgroup.com/sabanci/journalsearch?param_pattern_value=' + selectedText + '&param_textSearchType_value=contains';
+    // var serviceCall = 'https://sfx-90su.hosted.exlibrisgroup.com/sabanci/journalsearch?param_pattern_value=' + selectedText + '&param_textSearchType_value=contains';
+    var serviceCall = 'https://www.sabanciuniv.edu/bm/en/search-redirect?type=j&term=c&value=' + selectedText;
     chrome.tabs.create({
         url: serviceCall
     });
@@ -66,11 +80,20 @@ chrome.contextMenus.create({
 });
 
 chrome.contextMenus.create({
-    title: "Keyword",
+    title: "Subject",
     parentId: "parent",
     contexts: ["selection"],
     onclick: function (info, tab) {
-        catalogKeyword(info.selectionText);
+        catalogSubject(info.selectionText);
+    }
+});
+
+chrome.contextMenus.create({
+    title: "Any",
+    parentId: "parent",
+    contexts: ["selection"],
+    onclick: function (info, tab) {
+        catalogAny(info.selectionText);
     }
 });
 
@@ -97,3 +120,19 @@ chrome.contextMenus.create({
         journalContains(info.selectionText);
     }
 });
+
+
+function trackAction() {
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-35605398-1']);
+    _gaq.push(['_trackPageview']);
+    
+    (function () {
+        var ga = document.createElement('script');
+        ga.type = 'text/javascript';
+        ga.async = true;
+        ga.src = 'https://ssl.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(ga, s);
+    })();
+}
